90fec21edd1597770625ddce858aa309821562a3
Danilo Montecel <daniloomarmontecel@gmail.com>
2024-05-24 14:22:03 -0500

    version 2

--- src/components/Burguer.jsx
+++ src/components/Burguer.jsx
@@ -4,7 +4,7 @@ const Burguer = (props) => {
   return (
     <div>
       <div >
-        <button onClick={props.handleClick} id="btnOpen" aria-label="abrir menu" type="button">
+        <button className='crp' onClick={props.handleClick} id="btnOpen" aria-label="abrir menu" type="button">
         <i className="fa-solid fa-bars"></i></button>
         </div>
     </div>
